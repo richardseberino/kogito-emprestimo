b8d122938e0ba9226dde090e9939fd47bd6e1a1a
Richard Marques <seberino@MacBook-Pro-de-Richard.local>
2024-06-16 21:47:56 -0300

    inclusindo decisao no fluxo

---
+++ src/main/java/com/seberino/s2i/AvisaCliente.java
@@ -0,0 +1,17 @@
+package com.seberino.s2i;
+
+
+public class AvisaCliente 
+{
+
+    public void enviaEmail(String email, String mensagem)
+    {
+        System.out.println("Envia comunicado ao cliente! " + mensagem);
+    }
+
+
+    public void teste()
+    {
+        System.out.println("teste - Setvico");
+    }
+}
---
+++ src/main/java/com/seberino/s2i/Cliente.java
@@ -0,0 +1,39 @@
+package com.seberino.s2i;
+
+import java.io.Serializable;
+
+public class Cliente implements Serializable
+{
+
+    private String nome;
+    private String email;
+    private long cpf;
+    private double salario;
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public long getCpf() {
+        return cpf;
+    }
+    public void setCpf(long cpf) {
+        this.cpf = cpf;
+    }
+    public double getSalario() {
+        return salario;
+    }
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    
+}
