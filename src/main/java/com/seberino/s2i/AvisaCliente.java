9d76edf131378b121172d78747f512050cb0f7ab
Richard Marques <richard@seberino.com.br>
2024-06-20 08:49:23 -0300

    incluindo capacidade de email

--- src/main/java/com/seberino/s2i/AvisaCliente.java
+++ src/main/java/com/seberino/s2i/AvisaCliente.java
@@ -1,16 +1,26 @@
 package com.seberino.s2i;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import io.quarkus.mailer.Mail;
+import io.quarkus.mailer.Mailer;
 
 public class AvisaCliente 
 {
+
+    @Inject Mailer mail;   
 
     @ApplicationScoped
     public String enviaEmail(String email, String mensagem)
     {
         System.out.println("Envia comunicado ao cliente! " + mensagem);
-
+        //mail = new Mail();
+        mail.send(Mail.withText("seberino@hotmail.com", "teste de envio de email", "Mensagem de teste"));
         return "email enviado!";
     }
 
 }
+
+
+
